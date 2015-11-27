82c4c57ae528ae54778681b0abf2973d748f7f82
fengsenlin <fengsenlin@geotmt.com>
2015-11-27 19:59:58 +0800

    首次提交到本地

---
+++ src/main/java/eclipse_test/App.java
@@ -0,0 +1,14 @@
+package eclipse_test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.exit(0);
+    }
+}
